d31f84078cc44b208987728c25c6175e45dc2ca6
ujjwalKumar21 <131671973+ujjwalKumar21@users.noreply.github.com>
2023-05-23 17:34:03 +0530

    Update seller_verification.js

--- src/api/routes/v1/seller_verification.js
+++ src/api/routes/v1/seller_verification.js
@@ -5,6 +5,7 @@ const controller = require('../../controllers/seller_verification');
 const router = express.Router();
 
 router.get("/getData", controller.getData);
+router.get("/getSellerName", controller.getSellerName);
 router.get("/viewAttachments/:id", controller.getFileData);
 router.put("/isVerify", controller.updateData);
 router.delete("/deleteData", controller.deleteData);

8cb9d9fed0c9383b00a196d8d71bd54d6ed3a7e5
ujjwalKumar21 <131671973+ujjwalKumar21@users.noreply.github.com>
2023-05-23 11:59:34 +0530

    Update seller_verification.js

--- src/api/controllers/seller_verification.js
+++ src/api/controllers/seller_verification.js
@@ -18,7 +18,8 @@ module.exports = {
     let id = req.params.id;
     if(!id) return res.send("Id is required");
     try {
-        const data = await sellerInfoModel.find({_id: id},{_id:1, gstImageUrl: 1, panImageUrl: 1, email:1, fullname:1, description: 1, isVerify: 1, store_name: 1, mobile: 1, role: 1, marketplace: 1, storeDtl: 1, sellerType: 1});
+        const data = await sellerInfoModel.find({_id: id},{_id:1, gstImageUrl: 1, panImageUrl: 1,
+pan_number: 1, gst_number: 1, email:1, fullname:1, description: 1, isVerify: 1, store_name: 1, mobile: 1, role: 1, marketplace: 1, storeDtl: 1, sellerType: 1});
         if (!data || data.length == 0)
           return res.send({ msg: "No data is found" });
         return res.send(data);
